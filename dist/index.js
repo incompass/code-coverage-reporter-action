f0005a86278e243f6ba2252388bee76a9c3ac419
Mike Bates <mike@casechek.com>
2020-04-17 11:45:11 -0500

    feat: refactor coverage parsing

--- dist/index.js
+++ dist/index.js
@@ -556,54 +556,38 @@ const createCoverage = () => {
     );
     const coverageJson = JSON.parse(data);
 
-    // Get all the coverage values into human readable strings
-    let statements = `${coverageJson.total.statements.pct}% (${coverageJson.total.statements.covered}/${coverageJson.total.statements.total})`;
-    let branches = `${coverageJson.total.branches.pct}% (${coverageJson.total.branches.covered}/${coverageJson.total.branches.total})`;
-    let functions = `${coverageJson.total.functions.pct}% (${coverageJson.total.functions.covered}/${coverageJson.total.functions.total})`;
-    let lines = `${coverageJson.total.lines.pct}% (${coverageJson.total.lines.covered}/${coverageJson.total.lines.total})`;
-
+    let coverageString = '';
     let coverageResults = {
+        statements: '',
+        branches: '',
+        functions: '',
+        lines: '',
         thresholds: true,
         report: '',
     };
+    coverageJson.total.forEach((item, key) => {
+        // Get all the coverage values into human readable strings
+        coverageString = `${item.pct}% (${item.covered}/${item.total})`;
 
-    // Add thumbs down and mark tests as failed if anything is below threshold
-    if (coverageJson.total.statements.pct < coverageThreshold)  {
-        coverageResults.thresholds = false;
-        statements = `:thumbsdown: ${statements}`;
-    }
-    if (coverageJson.total.branches.pct < coverageThreshold)  {
-        coverageResults.thresholds = false;
-        branches = `:thumbsdown: ${branches}`;
-    }
-    if (coverageJson.total.functions.pct < coverageThreshold)  {
-        coverageResults.thresholds = false;
-        functions = `:thumbsdown: ${functions}`;
-    }
-    if (coverageJson.total.lines.pct < coverageThreshold)  {
-        coverageResults.thresholds = false;
-        lines = `:thumbsdown: ${lines}`;
-    }
+        // Add thumbs down and mark tests as failed if anything is below threshold
+        if (item.pct < coverageThreshold)  {
+            coverageResults.thresholds = false;
+            coverageString = `:thumbsdown: ${coverageString}`;
+        }
 
-    // Give anything that passed 100% an extra special emoji
-    if (coverageJson.total.statements.pct === 100)  {
-        statements = `:100: ${statements}`;
-    }
-    if (coverageJson.total.branches.pct === 100)  {
-        branches = `:100: ${branches}`;
-    }
-    if (coverageJson.total.functions.pct === 100)  {
-        functions = `:100: ${functions}`;
-    }
-    if (coverageJson.total.lines.pct === 100)  {
-        lines = `:100: ${lines}`;
-    }
+        // Give anything that passed 100% an extra special emoji
+        if (item.pct === 100)  {
+            coverageString = `:100: ${coverageString}`;
+        }
+
+        coverageResults[key] = coverageString;
+    });
 
     // Create the markdown for the comment
     coverageResults.report = `## Code Coverage Summary
 | Statements | Branch | Functions | Lines |
 |---|---|---|---|
-| ${statements} | ${branches} | ${functions} | ${lines}  |      
+| ${coverageResults.statements} | ${coverageResults.branches} | ${coverageResults.functions} | ${coverageResults.lines}  |      
 `;
 
     return coverageResults;
